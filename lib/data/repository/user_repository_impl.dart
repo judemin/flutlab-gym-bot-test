21c2164107ec5ea837a70144c8c85a922ab44366
MinSangYeon <judemin@konkuk.ac.kr>
2023-06-06 14:38:50 +0000

    [UPDATE] update user_repository_imple

--- lib/data/repository/user_repository_impl.dart
+++ lib/data/repository/user_repository_impl.dart
@@ -6,17 +6,18 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class UserRepositoryImpl implements UserRepository {
-  UserData? userData;
+  UserData _userData = UserData(email: "", password: "", name: "");
 
   @override
   void signUp(UserData userData) {
     // 데이터 소스와의 상호작용 및 회원가입 로직 구현
     // 예시로 데이터베이스에 새로운 사용자를 추가하는 로직을 작성
-    this.userData = userData;
+    this._userData = userData;
+    postRequest("https://gym-bot-backend.run.goorm.site/auth/signup");
   }
 
   Future<http.Response> postRequest(String url) async {
-    var body = userData.toJson();
+    var body = _userData.toJson();
 
     var response = await http.post(url,
         headers: {"Content-Type": "application/json"}, body: body);
--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       ),
       home: SignUpPage(
           signUpUseCase:
-              new SignUpUseCase(userRepository: new UserRepository())),
+              new SignUpUseCase(userRepository: UserRepositoryImpl())),
     );
   }
 }
